7ce34305001ab9d7720ce8e2eae99f59edaac937
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-05-18 05:43:35 +0000

    Update xml files..

---
+++ ExternalLogisticsAPI/LUM3DCartImportProc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using PX.Data;
+
+namespace ExternalLogisticsAPI
+{
+    public class LUM3DCartImportProc : PXGraph<LUM3DCartImportProc>
+    {
+
+        public PXSave<MasterTable> Save;
+        public PXCancel<MasterTable> Cancel;
+
+
+        public PXFilter<MasterTable> MasterView;
+        public PXFilter<DetailsTable> DetailsView;
+
+        [Serializable]
+        public class MasterTable : IBqlTable
+        {
+
+        }
+
+        [Serializable]
+        public class DetailsTable : IBqlTable
+        {
+
+        }
+    }
+}
