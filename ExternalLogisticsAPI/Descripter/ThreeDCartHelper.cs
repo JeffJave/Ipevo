ce9404036c28f24a6cc5d15fe5cf3aaaa731a753
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-06-02 12:24:40 +0000

    Add business account in the existing record check condition.

--- ExternalLogisticsAPI/Descripter/ThreeDCartHelper.cs
+++ ExternalLogisticsAPI/Descripter/ThreeDCartHelper.cs
@@ -247,7 +247,8 @@ namespace ExternalLogisticsAPI.Descripter
         {
             ContactMaint contactGraph = PXGraph.CreateInstance<ContactMaint>();
 
-            Contact origContact = SelectFrom<Contact>.Where<Contact.eMail.IsEqual<@P.AsString>>.View.Select(contactGraph, myArray.BillingEmail);
+            Contact origContact = SelectFrom<Contact>.Where<Contact.eMail.IsEqual<@P.AsString>
+                                                            .And<Contact.bAccountID.IsEqual<@P.AsInt>>>.View.Select(contactGraph, myArray.BillingEmail, sOCustomeID);
 
             if (origContact == null)
             {
