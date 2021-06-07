73b5b029a70199542f581a34d41e65c88f6ecc28
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-06-07 06:34:01 +0000

    Add unbound field to calculate the total instead of using standard field.

---
+++ IpevoCustomizations/DAC_Extensions/SOOrderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using PX.Data;
+
+namespace PX.Objects.SO
+{
+    public class SOOrderExt : PXCacheExtension<PX.Objects.SO.SOOrder>
+    {
+        // Jira IP-13
+        #region UsrCurySubTot
+        [PXDecimal()]
+        [PXUIField(DisplayName = "Sub Total", IsReadOnly = true)]
+        [PXFormula(typeof(Add<SOOrder.curyMiscTot, SOOrder.curyLineTotal>))]
+        public virtual decimal? UsrCurySubTot { get; set; }
+        public abstract class usrCurySubTot : PX.Data.BQL.BqlDecimal.Field<usrCurySubTot> { }
+        #endregion
+    }
+}
