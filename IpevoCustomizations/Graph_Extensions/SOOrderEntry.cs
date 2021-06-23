d1abb44ba0a68972471f20427d46001b2126eb01
Dennis <nirartha3214@gmail.com>
2021-06-23 17:48:31 +0800

    add report Prepayment Request

--- IpevoCustomizations/Graph_Extensions/SOOrderEntry.cs
+++ IpevoCustomizations/Graph_Extensions/SOOrderEntry.cs
@@ -1,10 +1,44 @@
 ﻿using PX.Data;
+using PX.Objects.AR;
+using PX.Objects.GL;
+using System.Collections;
+using System.Collections.Generic;
 using IpevoCustomizations.DAC;
 
 namespace PX.Objects.SO
 {
     public class SOOrderEntry_Extensions : PXGraphExtension<PX.Objects.SO.SOOrderEntry>
     {
+        public override void Initialize()
+        {
+            base.Initialize();
+            PrepaymentRequest.SetVisible(false);
+
+            var curCoutry = (PXSelect<Branch>.Select(Base, PX.Data.Update.PXInstanceHelper.CurrentCompany)).TopFirst;
+            if (curCoutry?.CountryID == "US" || curCoutry?.BaseCuryID == "USD")
+            {
+                PrepaymentRequest.SetVisible(true);
+                Base.report.AddMenuAction(PrepaymentRequest);
+            }
+        }
+
+        #region Action
+        public PXAction<SOOrder> PrepaymentRequest;
+        [PXButton]
+        [PXUIField(DisplayName = "Print Prepayment Request", Enabled = true, MapEnableRights = PXCacheRights.Select)]
+        protected virtual IEnumerable prepaymentRequest(PXAdapter adapter)
+        {
+            if (Base.Document.Current != null)
+            {
+                Dictionary<string, string> parameters = new Dictionary<string, string>();
+                parameters["OrderNbr"] = Base.Document.Current.OrderNbr;
+                parameters["OrderType"] = Base.Document.Current.OrderType;
+                throw new PXReportRequiredException(parameters, "LM641015", "Report LM641015");
+            }
+            return adapter.Get();
+        }
+        #endregion
+
         protected void _(Events.FieldUpdated<SOOrder.customerLocationID> e, PXFieldUpdated baseHandler)
         {
             baseHandler?.Invoke(e.Cache, e.Args);
