300cdd914028fba101f79a83792b7424baea12e2
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-06-30 06:06:41 +0000

    New requirement per YJ's request. It's copied from PX.AmazonIntegration project.

--- IpevoCustomizations/Graph_Extensions/SOInvoiceEntry_Extension.cs
+++ IpevoCustomizations/Graph_Extensions/SOInvoiceEntry_Extension.cs
@@ -1,6 +1,7 @@
 ﻿using PX.Data;
 using PX.Objects.AR;
 using PX.Objects.GL;
+using PX.Objects.TX;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -37,5 +38,54 @@ namespace PX.Objects.SO
             return adapter.Get();
         }
         #endregion
+
+        #region Cache Attached
+        [PXRemoveBaseAttribute(typeof(SOInvoiceTaxAttribute))]
+        [SOInvoiceTax2]
+        protected void _(Events.CacheAttached<ARTran.taxCategoryID> e) { }
+        #endregion
     }
+
+    #region Inherited class
+    public class SOInvoiceTax2Attribute : SOInvoiceTaxAttribute
+    {
+        protected override TaxDetail CalculateTaxSum(PXCache sender, object taxrow, object row)
+        {
+            TaxRev taxrev = PXResult.Unwrap<TaxRev>(taxrow);
+            Tax tax = PXResult.Unwrap<Tax>(taxrow);
+            
+            bool propagateCustomRate = false;
+            var origTaxRate = taxrev.TaxRate;
+
+            if (taxrev.TaxID != null && tax != null)
+            {
+                TaxExt taxExt = tax.GetExtension<TaxExt>();
+
+                if (taxExt.UsrAMPropagateTaxAmt == true)
+                {
+                    SOTaxTran soTax = PXResult<SOTaxTran>.Current;
+
+                    if (soTax != null && taxrev.TaxID == soTax.TaxID && soTax.CuryTaxableAmt.GetValueOrDefault() > 0)
+                    {
+                        var taxRate = soTax.CuryTaxAmt / soTax.CuryTaxableAmt * 100;
+                        if (taxRate != origTaxRate && taxRate > 0)
+                        {
+                            taxrev.TaxRate = taxRate;
+                            propagateCustomRate = true;
+                        }
+                    }
+                }
+            }
+
+            TaxDetail result = base.CalculateTaxSum(sender, taxrow, row);
+
+            if (result != null && propagateCustomRate)
+            {
+                result.TaxRate = origTaxRate;
+                taxrev.TaxRate = origTaxRate;
+            }
+            return result;
+        }
+    }
+    #endregion
 }
