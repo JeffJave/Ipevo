2632eb7dbda305e9daa6022042afda737f5bfe6b
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-07-08 13:55:31 +0000

    Add new middleware class entiry class.

---
+++ APILibrary/Model/Middleware_OpenInvoice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace APILibrary.Model
+{
+    public class Item
+    {
+        public string sku { get; set; }
+        public int qty { get; set; }
+        public double whTax { get; set; }
+    }
+
+    public class Root
+    {
+        public string description { get; set; }
+        public List<Item> item { get; set; }
+        public double paymentAmount { get; set; }
+        public double taxAmount { get; set; }
+    }
+}
