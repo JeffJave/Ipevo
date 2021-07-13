2acef00a5f6fe1b645c40f6464a468dea7350e4c
WayneLin <wayne.lin@lumina-tech.com>
2021-07-13 14:25:43 +0800

    1.新增PAC View DAC & Script

---
+++ ExternalLogisticsAPI/DAC/vPACUnitCost.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PX.Data;
+
+namespace ExternalLogisticsAPI
+{
+    [Serializable]
+    [PXCacheName("vPACUnitCost")]
+    public class vPACUnitCost : IBqlTable
+    {
+        #region InventoryID
+        [PXDBInt(IsKey = true)]
+        [PXUIField(DisplayName = "Inventory ID")]
+        public virtual int? InventoryID { get; set; }
+        public abstract class inventoryID : PX.Data.BQL.BqlInt.Field<inventoryID> { }
+        #endregion
+
+        #region Siteid
+        [PXDBInt(IsKey = true)]
+        [PXUIField(DisplayName = "Siteid")]
+        public virtual int? Siteid { get; set; }
+        public abstract class siteid : PX.Data.BQL.BqlInt.Field<siteid> { }
+        #endregion
+
+        #region FinPeriodID
+        [PXDBString(6, IsFixed = true, InputMask = "", IsKey = true)]
+        [PXUIField(DisplayName = "Fin Period ID")]
+        public virtual string FinPeriodID { get; set; }
+        public abstract class finPeriodID : PX.Data.BQL.BqlString.Field<finPeriodID> { }
+        #endregion
+
+        #region PACUnitCost
+        [PXDBDecimal()]
+        [PXUIField(DisplayName = "PACUnitCost")]
+        public virtual Decimal? PACUnitCost { get; set; }
+        public abstract class pACUnitCost : PX.Data.BQL.BqlDecimal.Field<pACUnitCost> { }
+        #endregion
+    }
+}
