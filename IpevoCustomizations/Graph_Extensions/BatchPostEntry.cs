c0108f2c9e385641de17b0634b5d312118bd7489
WayneLin <wayne.lin@lumina-tech.com>
2021-09-08 14:41:41 +0800

    完成GL Post Transaction Customization

---
+++ IpevoCustomizations/Graph_Extensions/BatchPostEntry.cs
@@ -0,0 +1,32 @@
+﻿using IpevoCustomizations.DAC_Extensions;
+using PX.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.GL
+{
+    public class BatchPostEntry : PXGraphExtension<BatchPost>
+    {
+        public override void Initialize()
+        {
+            base.Initialize();
+            Base.BatchList.SetProcessDelegate<PostGraph>(
+                delegate (PostGraph pg, Batch batch)
+                {
+                    var oldLastModifier = batch.LastModifiedByID;
+                    var oldLastModifiedTime = batch.LastModifiedDateTime;
+                    batch.GetExtension<BatchExtension>().UsrReviewer = oldLastModifier;
+                    batch.GetExtension<BatchExtension>().UsrReviewOn = oldLastModifiedTime;
+                    pg.Clear();
+                    pg.PostBatchProc(batch);
+                    //PXDatabase.Update<Batch>(new PXDataFieldAssign<BatchExtension.usrReviewer>(oldLastModifier),
+                    //                         new PXDataFieldRestrict<Batch.batchNbr>(batch.BatchNbr));
+                    //PXDatabase.Update<Batch>(new PXDataFieldAssign<BatchExtension.usrReviewOn>(oldLastModifiedTime),
+                    //                         new PXDataFieldRestrict<Batch.batchNbr>(batch.BatchNbr));
+                });
+        }
+    }
+}
---
+++ IpevoCustomizations/Graph_Extensions/JournalEntry.cs
@@ -0,0 +1,33 @@
+﻿using IpevoCustomizations.DAC_Extensions;
+using PX.Data;
+using PX.SM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Objects.GL
+{
+    public class JournalEntryExt : PXGraphExtension<JournalEntry>
+    {
+        public virtual void _(Events.RowSelected<Batch> e, PXRowSelected baseHandler)
+        {
+            baseHandler?.Invoke(e.Cache, e.Args);
+            if(e.Row != null)
+            {
+                if(e.Row.Status == BatchStatus.Unposted)
+                { 
+                    e.Row.GetExtension<BatchExtension>().UsrDisplayReviewer = Users.PK.Find(Base, e.Row.LastModifiedByID)?.FullName;
+                }
+                else if(e.Row.Status == BatchStatus.Posted)
+                {
+                    e.Row.GetExtension<BatchExtension>().UsrDisplayReviewer = Users.PK.Find(Base, e.Row.GetExtension<BatchExtension>().UsrReviewer)?.FullName;
+                    e.Row.GetExtension<BatchExtension>().UsrDisplayPostedBy = Users.PK.Find(Base,e.Row.LastModifiedByID)?.FullName;
+                }
+                else
+                    e.Row.GetExtension<BatchExtension>().UsrDisplayPostedBy = string.Empty;
+            }
+        }
+    }
+}
