cac0cb2dbe4e9e51429609f71f60047874389ba4
Administrator <Administrator@EC2AMAZ-DVS6HJL>
2021-10-05 14:19:51 +0000

    Add one more type for "FBA RMA RA(入庫日晚於付款日)"

--- ExternalLogisticsAPI/Descripter/AmazonFeeType.cs
+++ ExternalLogisticsAPI/Descripter/AmazonFeeType.cs
@@ -6,15 +6,16 @@ namespace ExternalLogisticsAPI.Descripter
     {
         public const int Amz_WithholdingTax = 1;
         public const int Amz_WarehouseFee = 2;
+        public const int Amz_Commission = 3;
 
         public static readonly int[] Values = new int[]
         {
-            Amz_WithholdingTax, Amz_WarehouseFee
+            Amz_WithholdingTax, Amz_WarehouseFee, Amz_Commission
         };
 
         public static readonly string[] Labels = new string[]
         {
-            "Amazon代徵代繳US州稅", "Amazon/Warehouse Fee"
+            "Amazon代徵代繳US州稅", "Amazon/Warehouse Fee", "Aamzon Commission"
         };
 
         public class ListAttribute : PXIntListAttribute
